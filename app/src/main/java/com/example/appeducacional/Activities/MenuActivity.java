eee0e7b585a351375acaec5143e3cfaf7f80a72c
AkiraHayato <38981764+AkiraHayato@users.noreply.github.com>
2019-06-11 22:29:04 -0300

    layout

--- app/src/main/java/com/example/appeducacional/Activities/MenuActivity.java
+++ app/src/main/java/com/example/appeducacional/Activities/MenuActivity.java
@@ -10,7 +10,7 @@ public class MenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_cadastro);
 
 
     }
