df2a1644150bb7ec546bf3808227493c503e2469
AkiraHayato <38981764+AkiraHayato@users.noreply.github.com>
2019-06-16 19:47:37 -0300

    update arqv
    classes

---
+++ app/src/main/java/com/example/appeducacional/Activities/MenuAdminActivity.java
@@ -0,0 +1,34 @@
+package com.example.appeducacional.Activities;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.appeducacional.R;
+
+import static com.example.appeducacional.R.menu.activity_admin;
+
+public class MenuAdminActivity extends AppCompatActivity {
+
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_admin);
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
